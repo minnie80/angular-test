98fd5e85076bb5d34bed3f1bce92dcee4a6cda6e
minnie80 <tienvuong2007@yahoo.com>
2023-08-17 09:13:25 +0200

    Added bootstrap from CDN. Got 'ReferenceError: bootstrap is not defined' with 'ng test' while 'ng serve' succeeded.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-test';
+  ngOnInit(){
+    console.log(bootstrap.Tooltip.VERSION);
+  }
 }
